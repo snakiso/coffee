6736d015d26ed011dc8d5c1f5895bac483d6a39c
snakiso <igorchagov@mail.ru>
2023-12-28 14:51:00 +0300

    fix HashRouter to browserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
 
     <Provider store={store}>
-        <BrowserRouter basename={`https://snakiso.github.io/coffee/`}>
+        <BrowserRouter>
             <App/>
         </BrowserRouter>
     </Provider>
